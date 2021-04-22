9a6ec5ab1ba121fd3360ea96c6f0a8e6228c1094
Marek Slavik <marek.slavik@strv.com>
2021-04-22 08:55:07 +0200

    Initial project state

--- CryptoMarketCap/CryptoMarketCap/Scenes/Markets/MarketsStore.swift
+++ CryptoMarketCap/CryptoMarketCap/Scenes/Markets/MarketsStore.swift
@@ -9,56 +9,5 @@ import Combine
 import SwiftUI
 
 final class MarketsStore: ObservableObject {
-    // State
-    enum State {
-        case initial
-        case loading
-        case ready(markets: [MarketItem])
-        case failed(error: Error)
-    }
-
-    // Public
-    @Published private(set) var state = State.initial
-
-    // Dependencies
-    private let marketsService: MarketsServicing = MarketsService.shared
-
-    // Private
-    private let refreshDataSubject = PassthroughSubject<Void, Never>()
-    private var cancellables = Set<AnyCancellable>()
-
-    init() {
-        refreshDataSubject
-            .handleEvents(receiveOutput: { [weak self] in
-                guard let self = self else { return }
-                switch self.state {
-                case .initial, .failed:
-                    // If state is not loaded yet, we want to show loading indicator.
-                    self.state = .loading
-                default:
-                    return
-                }
-            })
-            .compactMap { [weak self] _ -> AnyPublisher<State, Never>? in
-                self?.marketsService.markets()
-                    // Map data to state
-                    .map { markets -> State in
-                        State.ready(markets: markets)
-                    }
-                    // Map error to failed state
-                    .catch { error in
-                        Just(State.failed(error: error))
-                    }
-                    .eraseToAnyPublisher()
-            }
-            .switchToLatest()
-            .sink { [weak self] state in
-                self?.state = state
-            }
-            .store(in: &cancellables)
-    }
-
-    func loadData() {
-        refreshDataSubject.send()
-    }
+    
 }
