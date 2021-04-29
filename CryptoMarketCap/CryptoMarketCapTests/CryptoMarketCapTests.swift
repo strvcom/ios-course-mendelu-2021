9844feee5a4dfe44e4db08557dd365e7d9447677
Filip Haškovec <filip.haskovec@strv.com>
2021-04-29 12:33:17 +0200

    Test markets loading through MarketsService and mocked NetworkService.

--- CryptoMarketCap/CryptoMarketCapTests/CryptoMarketCapTests.swift
+++ CryptoMarketCap/CryptoMarketCapTests/CryptoMarketCapTests.swift
@@ -5,22 +5,46 @@
 //  Created by Marek Slávik on 12.04.2021.
 //
 
+import Combine
 import XCTest
 @testable import CryptoMarketCap
 
 class CryptoMarketCapTests: XCTestCase {
 
+    var sut: MarketsServicing?
+    var cancellables: Set<AnyCancellable> = []
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let mockNetwork = NetworkMock()
+        let coingeckoAPI = CoingeckoAPI(client: mockNetwork)
+
+        sut = MarketsService(coingeckoAPI: coingeckoAPI)
+        cancellables = []
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
+    func testMarkets() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let expectation = XCTestExpectation(description: "Markets should be fetched.")
+
+        sut?.markets()
+            .sink(receiveCompletion: { completion in
+                guard case .failure = completion else { return }
+
+                XCTFail("Market fetching shouldn't fail.")
+            }, receiveValue: { markets in
+                XCTAssertEqual(markets, NetworkMock.mockMarkets)
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 1)
     }
 
     func testPerformanceExample() throws {
--- CryptoMarketCap/CryptoMarketCapTests/Mocks/NetworkMock.swift
+++ CryptoMarketCap/CryptoMarketCapTests/Mocks/NetworkMock.swift
@@ -11,14 +11,17 @@ import Foundation
 
 class NetworkMock: Networking {
     func perform<T>(_ request: URLRequest, _ decoder: JSONDecoder) -> AnyPublisher<NetworkResponse<T>, Error> where T : Decodable {
-        guard T.self == MarketItem.self else {
+//            print(T.self is CoingeckoAPI.CoingeckoMarket)
+//            print(T.Type == CoingeckoAPI.CoingeckoMarket.Type)
+//            print(T.self is CoingeckoAPI.CoingeckoMarket)
+        guard T.self == [CoingeckoAPI.CoingeckoMarket].self else {
             return Empty(outputType: NetworkResponse<T>.self,
                          failureType: Error.self)
                 .eraseToAnyPublisher()
         }
 
         let mockedUrlResponse = URLResponse()
-        return Just(NetworkResponse<T>(value: mockMarkets as! T, response: mockedUrlResponse))
+        return Just(NetworkResponse<T>(value: mockNetworkMarkets as! T, response: mockedUrlResponse))
             .setFailureType(to: Error.self)
             .eraseToAnyPublisher()
     }
@@ -27,7 +30,28 @@ class NetworkMock: Networking {
 }
 
 extension NetworkMock {
-    var mockMarkets: [MarketItem] {
+    var mockNetworkMarkets: [CoingeckoAPI.CoingeckoMarket] {
+        [
+            CoingeckoAPI.CoingeckoMarket(id: "bitcoin",
+                       name: "Bitcoin",
+                       symbol: "btc",
+                       image:"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+                       currentPrice: 1152498,
+                       marketCap: 21545029536768,
+                       marketCapRank: 1,
+                       priceChangePercentage24h: -1.39861),
+            CoingeckoAPI.CoingeckoMarket(id: "ethereum",
+                       name: "Ethereum",
+                       symbol: "eth",
+                       image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+                       currentPrice: 58070,
+                       marketCap: 6714374876849,
+                       marketCapRank: 2,
+                       priceChangePercentage24h: 3.34647)
+        ]
+    }
+
+    static var mockMarkets: [MarketItem] {
         [
             MarketItem(id: "bitcoin",
                        name: "Bitcoin",
